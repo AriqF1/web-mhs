d043974fd80286ccd8127efe0c7019193f55ab3c
Farhan Ariq <122168324+AriqF1@users.noreply.github.com>
2025-02-27 21:43:27 +0700

    add: dashboard And refactor: optimasi script.js

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,59 @@
 function handleLogin(event){
     event.preventDefault();
 
-    let email = document.getElementById("email").value;
-    let password = document.getElementById("password").value;
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value.trim();
 
     if(!email || !password){
-        alert("Email Dan Password Wajib Diisi");
+        alert("Email dan Password wajib diisi");
         return;
     }
-        alert("Login Berhasil");
+        alert("Login berhasil");
+}
+
+function handleForm(event, actionType) {
+    event.preventDefault();
+
+    const nim = document.getElementById("nim").value.trim();
+    const nama = document.getElementById("nama").value.trim();
+
+    if (!nim || !nama) {
+        alert("NIM dan Nama wajib diisi");
+        return;
+    }
+    alert(`Mahasiswa berhasil ${actionType}`);
+}
+
+
+document.addEventListener("DOMContentLoaded", async function () {
+    async function loadComponent(elementId, filePath) {
+        try {
+            const response = await fetch(filePath);
+            if (!response.ok) throw new Error(`Gagal memuat ${filePath}`);
+            
+            const data = await response.text();
+            document.getElementById(elementId).innerHTML = data;
+        } catch (error) {
+            console.error(`Error loading ${filePath}:`, error);
+        }
+    }
+
+    // load sidebar dan header
+    loadComponent("sidebar-container", "component/sidebar.html");
+    loadComponent("header-container", "component/header.html");
+    loadComponent("footer-container", "component/footer.html");
+    loadComponent("add-modal", "assets/addModal.html");
+    loadComponent("edit-modal", "assets/editModal.html");
+});
+
+function openModal(modalId) {
+    document.getElementById(modalId).classList.remove("hidden");
+}
+
+function closeModal(modalId) {
+    document.getElementById(modalId).classList.add("hidden");
+}
+
+function ubahTeksModal(teksBaru) {
+    document.getElementById("judul-modal").textContent = teksBaru;
 }
