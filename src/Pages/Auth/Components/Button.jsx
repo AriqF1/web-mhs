2874e918cb68b03a60ad3e15460007b7b2a675d8
Ariq Farhan <ariqalthaf445@gmail.com>
2025-05-05 11:33:18 +0700

    Add: enhance Button and Input components to accept custom className for styling

--- src/Pages/Auth/Components/Button.jsx
+++ src/Pages/Auth/Components/Button.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 
-const Button = ({type ="submit", children}) => {
-    return (
-        <button type={type} className="w-full py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition">
-            {children}
-        </button>
-    );
+const Button = ({ type, children, className }) => {
+  return (
+    <button
+      type={type}
+      className={
+        "w-full py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition ${className}"
+      }
+    >
+      {children}
+    </button>
+  );
 };
 export default Button;
