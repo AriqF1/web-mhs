9d4d125d3f3b6094e32f0c625b67fddf7f436274
Ariq Farhan <ariqalthaf445@gmail.com>
2025-06-04 22:16:12 +0700

    fix: update database import in dosen route to use knexfile

--- backend/routes/dosen.js
+++ backend/routes/dosen.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const db = require("../db");
+const db = require("../knexfile");
 
 // GET semua dosen
 router.get("/", (req, res) => {
