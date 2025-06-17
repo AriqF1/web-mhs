73118dcc728780bc6c12626215c0802818d550c2
Ariq Farhan <ariqalthaf445@gmail.com>
2025-06-17 22:13:53 +0700

    feat: add matakuliah routes and migration for matakuliah table

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const dosenRoutes = require("./routes/dosen");
 const prodiRoutes = require("./routes/prodi");
+const matkulRoutes = require("./routes/matkul");
 
 const app = express();
 const PORT = 3000;
@@ -12,6 +13,7 @@ app.use(express.json());
 
 app.use("/api/dosen", dosenRoutes);
 app.use("/api/prodi", prodiRoutes);
+app.use("/api/matkul", matkulRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
