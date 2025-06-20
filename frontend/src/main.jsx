70172c491a74af0374f628d93a0851d7f97deffe
Ariq Farhan <ariqalthaf445@gmail.com>
2025-06-20 21:13:37 +0700

    feat: wrap AppRoutes and ToastContainer with AuthProvider for context management

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,18 +4,21 @@ import "./App.css";
 import AppRoutes from "./AppRoutes.jsx";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { AuthProvider } from "../AuthContext";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AppRoutes />
-    <ToastContainer
-      position="top-right"
-      autoClose={3000}
-      hideProgressBar={false}
-      newestOnTop={false}
-      closeOnClick
-      rtl={false}
-      theme="light"
-    />
+    <AuthProvider>
+      <AppRoutes />
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        theme="light"
+      />
+    </AuthProvider>
   </StrictMode>
 );
