b9ab1a62e5d67b80954195668d32b648a2839619
Ariq Farhan <ariqalthaf445@gmail.com>
2025-06-20 23:02:04 +0700

    feat: update DetailDosen and DosenForm components to remove unused fields and improve data handling

--- frontend/src/pages/components/organism/DetailDosen.jsx
+++ frontend/src/pages/components/organism/DetailDosen.jsx
@@ -1,6 +1,5 @@
 import AvatarPlaceholder from "../atoms/AvatarPlaceholder";
 import LabelValue from "../atoms/LabelValue";
-import StatusBadge from "../atoms/StatusBadge";
 
 const DetailDosen = ({ dsn }) => (
   <div className="space-y-4">
@@ -10,12 +9,8 @@ const DetailDosen = ({ dsn }) => (
     <div className="grid grid-cols-2 gap-4">
       <LabelValue label="NIP" value={dsn.nip} />
       <LabelValue label="Nama" value={dsn.nama} />
-      <LabelValue label="Email" value={dsn.email} />
       <LabelValue label="Prodi" value={dsn.prodi} />
-      <div>
-        <h4 className="text-sm font-medium text-gray-500">Status</h4>
-        <StatusBadge status={dsn.status} />
-      </div>
+      <LabelValue label="Max Sks" value={dsn.max_sks} />
     </div>
   </div>
 );
