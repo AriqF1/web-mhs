52195226693dc4fec598a60414eca65d826e54e0
Ariq Farhan <ariqalthaf445@gmail.com>
2025-06-21 12:33:44 +0700

    feat: wrap application in QueryClientProvider for React Query integration

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,20 +5,24 @@ import AppRoutes from "./AppRoutes.jsx";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { AuthProvider } from "../AuthContext";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AuthProvider>
-      <AppRoutes />
-      <ToastContainer
-        position="top-right"
-        autoClose={3000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        theme="light"
-      />
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <AppRoutes />
+        <ToastContainer
+          position="top-right"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          theme="light"
+        />
+      </AuthProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
